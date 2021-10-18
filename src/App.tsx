c5bea22394876bb3bdacc88cedf425d197c6a3fd
Chen Lee <chenlee@ChendeMacBook-Pro.local>
2021-10-18 19:41:44 +0800

    first init -- install tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import tw from 'twin.macro';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Wrapper = tw.div`
+  flex
+  w-full
+  bg-blue-300
+`
+
+
+
+const App = (): JSX.Element => (<Wrapper>Test</Wrapper>)
 
 export default App;
